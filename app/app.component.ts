54e1312eec479842839d546fee02feadd483e5b1
jose <josehilariomontesricardo@gmail.com>
2016-09-18 16:09:09 -0500

    [modify] app.componnet.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,11 @@ import  { Component } from '@angular/core';
 export class Person {
 	id 		: number;
 	name 	: string;
+
+	constructor(idParam: number, nameParam: string){
+		this.id 		= idParam;
+		this.name 		= nameParam;
+	}
 }
 
 @Component({
@@ -10,14 +15,10 @@ export class Person {
 	template 	: 	`<h1>
 						{{ title }}
 					</h1>
-					<h2> {{ person.id }}  {{ person.name }} </h2>
-					`
+					<h2> {{ person.id }}  {{ person.name }} </h2>`
 })
 
 export class AppComponent{ 
 	title	= 'lista de personas';
-	person  : Person = {
-				id 		: 1,
-				name 	: 'dennis ritchie'
-			};
+	person  = new Person(1, 'Dennis ritchie');
 }
