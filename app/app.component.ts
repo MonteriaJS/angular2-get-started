28d782cf69f6562bf74aedee8b2f0b1f636a4e7a
jose <josehilariomontesricardo@gmail.com>
2016-09-19 16:51:44 -0500

    [modify] app.component.ts, add personArray

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,11 +10,22 @@ export class Person {
 	}
 }
 
+const personsArray : Person[] = [
+	{ id: 1, name: 'Dennis ritchie' },
+	{ id: 2, name: 'Linus Torvalds' },
+	{ id: 3, name: 'Richard Stallman' },
+	{ id: 4, name: 'Steve Jobs' },
+	{ id: 5, name: 'Bill Gates' }
+];
+
 @Component({
 	selector	: 'my-app',
 	template 	: 	`<h1>
 						{{ title }}
 					</h1>
+					<ul *ngFor=" let person of persons ">
+						<li > {{person.id}} {{person.name}} </li>
+					</ul>
 					<h2> {{ person.id }}  {{ person.name }} </h2>
 					<input [(ngModel)]="person.name" placeholder="name">`
 })
@@ -22,4 +33,5 @@ export class Person {
 export class AppComponent{ 
 	title	= 'lista de personas';
 	person  = new Person(1, 'Dennis ritchie');
+	persons = personsArray;
 }
